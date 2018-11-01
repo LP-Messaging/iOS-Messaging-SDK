9f00d6a9bf97cbd6cb5c5bb7a6299a309374a6cc
Asaf Inbar <asafi@liveperson.com>
2018-11-01 10:10:25 +0200

    update frameworks to version 3.4.0

---
+++ Libraries/LPInfra.framework/Headers/LPInfra.h
@@ -0,0 +1,19 @@
+//
+//  LPInfra.h
+//  LPInfra
+//
+//  Created by Nimrod Shai on 12/21/15.
+//  Copyright © 2015 Udi Dagan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Infra.
+FOUNDATION_EXPORT double InfraVersionNumber;
+
+//! Project version string for Infra.
+FOUNDATION_EXPORT const unsigned char InfraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Infra/PublicHeader.h>
+
+
