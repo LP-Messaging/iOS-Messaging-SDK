cb4868c44a971428ede38211e69caaac80c9a176
UMAIR Ali <umali@liveperson.com>
2022-06-10 15:42:12 -0500

    release version 6.8.2.837

---
+++ Libraries/LPMessagingSDK.xcframework/LPMessagingSDK.xcframework/ios-arm64/LPMessagingSDK.framework/Headers/LPMessagingSDK.h
@@ -0,0 +1,23 @@
+//
+//  LPMessagingSDK.h
+//  LPMessagingSDK
+//
+//  Created by Udi Dagan on 8/9/15.
+//  Copyright (c) 2015 Udi Dagan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+//------ We are using 3rd party reachability class as well
+
+
+//TODO: Add to integration doc: include systemConfiguration.framework (for reachability)
+
+//! Project version number for LPMessagingSDK.
+FOUNDATION_EXPORT double LPMessagingSDKVersionNumber;
+
+//! Project version string for LPMessagingSDK.
+FOUNDATION_EXPORT const unsigned char LPMessagingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LPMessagingSDK/PublicHeader.h>
